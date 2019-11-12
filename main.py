763e851be5786ac937133b8a6d6cd6536739cbb6
vapvin <tpehdj12@daum.net>
2019-11-12 12:22:57 +0900

    Today For 30

---
+++ main.py
@@ -0,0 +1,86 @@
+print("Hello World")
+print("Mary's cosmetics")
+
+print('신씨가 소리질렀다. "도둑이야".')
+print('"C:\\Windows"')
+
+print("안녕하세요.\n만나서\t\t반갑습니다.")
+# \n 줄바꿈 \t 들여쓰기
+
+print("오늘은", "일요일")
+
+print("naver","kakao","sk","samsung", sep=";")
+print("naver","kakao","sk","samsung", sep="/")
+print("first", end=" ");print("second")
+
+string = "dk2jd923i1dk2jd93jfd92jd918943jfd8923"
+
+print(len(string))
+
+a = "3"
+b = "4"
+
+print(a+b)
+
+s = "hello"
+t = "python"
+
+print(s,end="! ");print(t)
+
+print("Hi"*3)
+print("-"*80)
+
+t1 = 'python'
+t2 = 'java'
+
+print((t1+' '+t2+' ')*4)
+
+print(20000*10)
+
+print(2 + 2 * 3)
+
+f = "132"
+
+print(type(f))
+
+num_str = "720"
+num_str = int(num_str)
+
+num = 100
+num = str(num)
+
+lang = 'python'
+print(lang[0], lang[2])
+
+license_plate = "24가 2210"
+print(license_plate[4:])
+
+string_odd = "홀짝홀짝홀짝"
+print(string_odd[::2])
+print(string_odd[1::2])
+
+string_revers = "PYTHON"
+print(string_revers[::-1])
+
+phone_number = "010-1111-2222"
+print(phone_number[:3],phone_number[4:8],phone_number[9:13])
+print(phone_number.replace('-', ' '))
+print(phone_number.replace('-', ''))
+
+url = "http://sharebook.kr"
+print(url[-2:])
+
+lang_py = 'python'
+print(lang_py)
+
+# 만들어진 문자열은 바인딩 불가
+
+english = "abcdfe2a354a32a"
+print(english.replace('a', 'A'))
+
+abc = 'abcd'
+new_abc = abc.replace('b', 'B')
+print(new_abc)
+
+
+
