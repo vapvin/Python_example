99742bc39211e88b30c83677f60102278495a7df
vapvin <tpehdj12@daum.net>
2019-11-15 14:19:20 +0900

    ~50 example complete

--- main.py
+++ main.py
@@ -104,5 +104,20 @@ lang1 = ["c", "c++", "JAVA"]
 lang2 = ["Python", "Go", "C#"]
 
 langs = lang1 + lang2
+print(langs)
 
+nums = [1,2,3,4,5,6,7]
+
+print("max:", max(nums))
+print("min:", min(nums))
+
+print(sum(nums))
+
+cook = ["피자", "김밥", "만두", "양념치킨", "족발", "피자", "김치만두", "쫄면", "쏘세지", "라면", "팥빙수", "김치전"]
+
+print(len(cook))
+
+numis = [1,2,3,4,5]
+
+print(sum(numis)/len(numis))
 
