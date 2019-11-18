28b0b295ae1e8b237f54287dd36f319ac994322f
vapvin <tpehj12@daum.net>
2019-11-18 09:24:49 +0900

    Dictionary

--- main.py
+++ main.py
@@ -184,3 +184,25 @@ print(valid_score2)
 temp = {
 
 }
+
+
+icecream = {
+    "메로나": 1000,
+    "폴라포": 1200,
+    "빵빠레": 1800
+}
+
+print(icecream)
+
+icecream['죠스바'] = 1200
+icecream['월드콘'] = 1500
+
+print(icecream)
+
+icecream['메로나'] = 1300
+
+print(icecream)
+
+del icecream['메로나']
+
+print(icecream)
