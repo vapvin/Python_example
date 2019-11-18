5ab143bc07b2b937f1b74f3357e9b24bc209ea08
vapvin <tpehj12@daum.net>
2019-11-18 09:34:00 +0900

    Dict Val and Keys

--- main.py
+++ main.py
@@ -206,3 +206,34 @@ print(icecream)
 del icecream['메로나']
 
 print(icecream)
+
+inventory = {
+    '메로나': [
+        300, 20
+    ],
+    '비비빅': [
+        400, 3
+    ],
+    '죠스바': [
+        250, 1000
+    ],
+}
+
+print(inventory)
+
+print(inventory['메로나'][0], '원')
+print(inventory['메로나'][1], '개')
+
+inventory['월드콘'] = [500, 7]
+
+print(inventory)
+
+icecream = {'탱크보이': 1200, '폴라포': 1200, '빵빠레': 1800, '월드콘': 1500, '메로나': 1000}
+
+dict_keys = list(icecream.keys())
+
+print(dict_keys)
+
+dict_val = list(icecream.values())
+
+print(dict_val)
