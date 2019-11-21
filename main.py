ebf28824cab7c9143afa413d5748d3eb0f2a5e52
vapvin <tpehdj12@daum.net>
2019-11-21 14:05:32 +0900

    This Grade

--- main.py
+++ main.py
@@ -391,3 +391,18 @@ elif 21 <= user_score <= 40:
 else:
     print("grad is E")
 
+user_in = input("입력:").split()
+
+amount = user_in[0]
+currency = user_in[1]
+
+if currency == "달라":
+    ratio = 1167
+elif currency == "엔":
+    ratio = 1.096
+elif currency == "유로":
+    ratio = 1268
+else:
+    ratio = 171
+
+print(ratio * int(amount), "원")
