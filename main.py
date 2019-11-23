bd5546ff1a3dcb424b0fb167d7609e5726d4d1f3
vapvin <tpehdj12@daum.net>
2019-11-23 18:28:13 +0900

    Class New Study

--- main.py
+++ main.py
@@ -649,3 +649,23 @@ def messages3():
 
 messages3()
 
+
+class Flight:
+
+    def __init__(self):
+        print('init')
+        super().__init__()
+
+    def __new__(cls):
+        print('new')
+        return super().__new__(cls)
+
+    def number(self):
+        return 'SN060'
+
+class Flight:
+    def __init__(self, number):
+        self._number = number
+
+    def number(self):
+        return self._number
